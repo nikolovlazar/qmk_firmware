5d4138c36222b2f9d4a001a6bf0c1deb823d564e
Lazar Nikolov <hello@nikolovlazar.com>
2026-02-12 11:34:38 -0500

    stripping down extra keys

--- keyboards/keychron/q8/ansi_encoder/keymaps/nikolovlazar/keymap.c
+++ keyboards/keychron/q8/ansi_encoder/keymaps/nikolovlazar/keymap.c
@@ -31,11 +31,11 @@ enum layers{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_69(
-        KC_EQL,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
-        KC_ESC, MT(MOD_LCTL, KC_A),     MT(MOD_LALT, KC_S),     MT(MOD_LGUI, KC_D),    MT(MOD_LSFT, KC_F),    KC_G,              KC_H,    MT(MOD_RSFT, KC_J),    MT(MOD_RGUI, KC_K),    MT(MOD_RALT, KC_L),     MT(MOD_RCTL, KC_SCLN),  KC_QUOT,  KC_ENT,           KC_HOME,
-        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPT,  KC_LCMD,           KC_SPC,           MO(_FN1), MO(_FN3),         KC_SPC,            KC_RCMD,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_EQL, KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  XXXXXXX, XXXXXXX,          KC_MUTE,
+        KC_TAB, KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  XXXXXXX, XXXXXXX,          XXXXXXX,
+        KC_ESC, MT(MOD_LCTL, KC_A),     MT(MOD_LALT, KC_S),     MT(MOD_LGUI, KC_D),    MT(MOD_LSFT, KC_F),   KC_G,     KC_H,    MT(MOD_RSFT, KC_J),    MT(MOD_RGUI, KC_K),    MT(MOD_RALT, KC_L),     MT(MOD_RCTL, KC_SCLN),  KC_QUOT,  XXXXXXX, XXXXXXX,
+        XXXXXXX,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT ,   KC_SLSH,  XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX,  XXXXXXX,        KC_SPC,             MO(_FN1), KC_ENT, MO(_FN3),  XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX),
 
     [WIN_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
@@ -83,56 +83,70 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     uint8_t layer = get_highest_layer(layer_state);
 
-    if (layer == 0) {
-        // Layer 0: Default rainbow animation
-        return false;
+    if (layer > 0) {
+        // Turn off all LEDs for non-base layers
+        for (uint8_t i = led_min; i < led_max; i++) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        }
+
+        if (layer == 2) {
+            rgb_matrix_set_color(4, 1, 238, 255);
+            rgb_matrix_set_color(5, 1, 238, 255);
+            rgb_matrix_set_color(6, 1, 238, 255);
+            rgb_matrix_set_color(7, 1, 238, 255);
+            rgb_matrix_set_color(8, 1, 238, 255);
+
+            rgb_matrix_set_color(20, 66, 255, 0);
+
+            rgb_matrix_set_color(32, 245, 9, 9);
+            rgb_matrix_set_color(33, 0, 14, 255);
+            rgb_matrix_set_color(34, 0, 14, 255);
+            rgb_matrix_set_color(35, 66, 255, 0);
+
+            rgb_matrix_set_color(46, 245, 9, 9);
+            rgb_matrix_set_color(47, 0, 14, 255);
+            rgb_matrix_set_color(48, 0, 14, 255);
+            rgb_matrix_set_color(49, 66, 255, 0);
+        } else if (layer == 4) {
+            rgb_matrix_set_color(16, 255, 255, 255);
+            rgb_matrix_set_color(17, 255, 255, 255);
+            rgb_matrix_set_color(18, 255, 255, 255);
+            rgb_matrix_set_color(19, 255, 255, 255);
+            rgb_matrix_set_color(22, 255, 255, 255);
+            rgb_matrix_set_color(23, 255, 255, 255);
+            rgb_matrix_set_color(24, 255, 255, 255);
+            rgb_matrix_set_color(25, 255, 255, 255);
+
+            rgb_matrix_set_color(31, 255, 255, 255);
+            rgb_matrix_set_color(36, 245, 9, 9);
+            rgb_matrix_set_color(37, 245, 9, 9);
+            rgb_matrix_set_color(38, 245, 9, 9);
+            rgb_matrix_set_color(39, 245, 9, 9);
+
+            rgb_matrix_set_color(45, 255, 255, 255);
+        }
     }
 
-    // Turn off all LEDs
-    for (uint8_t i = led_min; i < led_max; i++) {
-        rgb_matrix_set_color(i, 0, 0, 0);
-    }
+    // Always turn off disabled keys (keycaps removed)
+    rgb_matrix_set_color(12, 0, 0, 0);
+    rgb_matrix_set_color(13, 0, 0, 0);
+    rgb_matrix_set_color(27, 0, 0, 0);
+    rgb_matrix_set_color(28, 0, 0, 0);
+    rgb_matrix_set_color(29, 0, 0, 0);
+    rgb_matrix_set_color(42, 0, 0, 0);
+    rgb_matrix_set_color(43, 0, 0, 0);
+    rgb_matrix_set_color(44, 0, 0, 0);
+    rgb_matrix_set_color(56, 0, 0, 0);
+    rgb_matrix_set_color(57, 0, 0, 0);
+    rgb_matrix_set_color(58, 0, 0, 0);
+    rgb_matrix_set_color(59, 0, 0, 0);
+    rgb_matrix_set_color(60, 0, 0, 0);
+    rgb_matrix_set_color(65, 0, 0, 0);
+    rgb_matrix_set_color(66, 0, 0, 0);
+    rgb_matrix_set_color(67, 0, 0, 0);
+    rgb_matrix_set_color(68, 0, 0, 0);
 
-    if (layer == 2) {
-        // Layer 1: Light specific keys (replace indices with actual values)
-        rgb_matrix_set_color(4, 1, 238, 255);
-        rgb_matrix_set_color(5, 1, 238, 255);
-        rgb_matrix_set_color(6, 1, 238, 255);
-        rgb_matrix_set_color(7, 1, 238, 255);
-        rgb_matrix_set_color(8, 1, 238, 255);
-
-        rgb_matrix_set_color(20, 66, 255, 0);
-
-        rgb_matrix_set_color(32, 245, 9, 9);
-        rgb_matrix_set_color(33, 0, 14, 255);
-        rgb_matrix_set_color(34, 0, 14, 255);
-        rgb_matrix_set_color(35, 66, 255, 0);
-
-        rgb_matrix_set_color(46, 245, 9, 9);
-        rgb_matrix_set_color(47, 0, 14, 255);
-        rgb_matrix_set_color(48, 0, 14, 255);
-        rgb_matrix_set_color(49, 66, 255, 0);
-    } else if (layer == 4) {
-        // Layer 2: Light specific keys (replace indices with actual values)
-        rgb_matrix_set_color(16, 255, 255, 255);
-        rgb_matrix_set_color(17, 255, 255, 255);
-        rgb_matrix_set_color(18, 255, 255, 255);
-        rgb_matrix_set_color(19, 255, 255, 255);
-        rgb_matrix_set_color(22, 255, 255, 255);
-        rgb_matrix_set_color(23, 255, 255, 255);
-        rgb_matrix_set_color(24, 255, 255, 255);
-        rgb_matrix_set_color(25, 255, 255, 255);
-
-        rgb_matrix_set_color(31, 255, 255, 255);
-        rgb_matrix_set_color(36, 245, 9, 9);
-        rgb_matrix_set_color(37, 245, 9, 9);
-        rgb_matrix_set_color(38, 245, 9, 9);
-        rgb_matrix_set_color(39, 245, 9, 9);
-
-        rgb_matrix_set_color(45, 255, 255, 255);
-    }
-
-    return true;
+    return false;
 }
 
 #if defined(ENCODER_MAP_ENABLE)
